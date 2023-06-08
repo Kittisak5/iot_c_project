7b087568b107029f19af1b61b36a2dbb7740605c
Kittisak5 <s6519n10004@sau.ac.th>
2023-06-08 22:19:45 +0700

    Version3.....

--- practice01/pt02.c
+++ practice01/pt02.c
@@ -1,5 +1,23 @@
-//โจทย์ข้อที่2
-main(){
-    
+#include<stdio.h>
+#define wow printf("+++++++++++++++++++++++++++++\n");
 
+void main(){
+    float widths,longs,highs,color_use;
+
+wow
+printf("Program Color\n");
+wow
+printf("Input widths: ");
+    scanf("%f",&widths);
+wow
+printf("Input longs: ");
+    scanf("%f",&longs);
+wow
+ printf("Input highs: ");
+    scanf("%f",&highs);    
+wow
+color_use = widths * longs / 3;
+printf(" Color Use: %.2f\n",color_use);
+
+getch();
 }
