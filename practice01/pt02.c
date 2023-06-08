08407378ef73f25a3147b24a9c0847ca037e1eac
Kittisak5 <s6519n10004@sau.ac.th>
2023-06-08 22:25:26 +0700

    commit Version 4 complete

--- practice01/pt02.c
+++ practice01/pt02.c
@@ -1,23 +1,26 @@
-#include<stdio.h>
-#define wow printf("+++++++++++++++++++++++++++++\n");
+#include "stdio.h"
+#include "conio.h"
 
-void main(){
-    float widths,longs,highs,color_use;
-
-wow
-printf("Program Color\n");
-wow
-printf("Input widths: ");
-    scanf("%f",&widths);
-wow
-printf("Input longs: ");
-    scanf("%f",&longs);
-wow
- printf("Input highs: ");
-    scanf("%f",&highs);    
-wow
-color_use = widths * longs / 3;
-printf(" Color Use: %.2f\n",color_use);
-
-getch();
+#define PA printf("======================================================\n");
+void main (){
+    float widths,longs,hights,wh_area , wh_areamax,lh_area, lh_areamax,lw_area, lw_areamax ,area_max, color_used;
+    int x = 2  ;
+    PA
+    printf("Input Width:");
+    scanf("%f", &widths);
+    printf("Input Long:");
+    scanf("%f", &longs);
+    printf("Input Hight:");
+    scanf("%f", &hights);
+    wh_area = widths * hights;
+    wh_areamax = wh_area * x ;
+    lh_area = longs * hights ;
+    lh_areamax = lh_area * x ;
+    lw_area = longs * widths ;
+    lw_areamax = lw_area * x ;
+    area_max = wh_areamax + lh_areamax +lw_areamax;
+    color_used = area_max / 3 ;
+    printf("Color for use is:%.2f\n",color_used);
+    PA
+    getch();
 }
